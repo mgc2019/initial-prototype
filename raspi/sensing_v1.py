422ef8519345033ca243814b2448d08d0995ec63
yewwoei <yewwoei.low@outlook.com>
2019-01-08 15:10:52 +0000

    add multithreading capability to the script in preparation for driving multiple motor-ultrasound pairs

--- raspi/sensing_v1.py
+++ raspi/sensing_v1.py
@@ -1,16 +1,32 @@
-# authors: Aiden Low Yew Woei
+"""Environment sensing script for the blind, version one.
 
-# This python script makes use of four ultrasound-vibration sensors.
+This python script is written modularly and allows the use of
+multiple ultrasound-vibration sensors.
+
+Multithreading is used to minimise downtime between each ultrasound detection loop.
+
+Why multithreading instead of multiprocessing?
+    Multithreading has lower overhead. All threads share the same memory space.
+    No new ones have to be allocated and managed for new commands.
+    The above also means that there won't be issues controlling different GPIO
+    pins tied to global variables, as compared to using processes which will
+    operate on a different memory.
+
+Todo:
+    * implement threads for all other directions in line with prototype.
+    * decide on feedback algorithm for ceiling feedback.
+"""
 
 from gpiozero import InputDevice, OutputDevice, PWMOutputDevice
 from math import e
-from multiprocessing import Process
+from threading import Thread
 from time import sleep, time
 
 # constants
 SPEED_OF_SOUND_CM = 34300
 ULTRASOUND_MAX_RANGE = 400
 ULTRASOUND_MIN_RANGE = 2
+
 # see wolfram alpha for the decay curve.
 VIBRATION_EXPONENTIAL_DECAY_CONSTANT = 200
 
@@ -25,18 +41,28 @@ sleep(1)
 print("Set-up input and output devices")
 
 def get_pulse_time(trig, echo):
+    """Find the time of flight using ultrasound.
+    
+    Keyword arguments:
+    trig -- the GPIO pin controlling the ultrasound trigger
+    echo -- the GPIO pin that gives the return signal of the reflected echo
+
+    An ultrasound burst of 10 microseconds is used.
+    """
+    
+    # send the ultrasound burst
     trig.on()
-    # send a burst of ultrasound for 10 microsends
     sleep(0.00001)
     trig.off()
     
+    # calculate time of flight
     while echo.is_active == False:
         pulse_start = time()
 
     while echo.is_active == True:
         pulse_end = time()
 
-    # Let the ultrasound sleep for abit.
+    # Let the ultrasound sleep to 
     sleep(0.10)
 
     # pulse_start or pulse_end may occasionally have problems with cycle of trigger
@@ -46,10 +72,11 @@ def get_pulse_time(trig, echo):
     except:
         return 0.02
 
-# calculate distance in metres
 def calculate_distance(duration):
+    """Calculates distance and returns it in centimetres"""
     distance = SPEED_OF_SOUND_CM * duration / 2
 
+    # accounting for ultrasound time of flight range errors
     if distance > ULTRASOUND_MAX_RANGE:
         return ULTRASOUND_MAX_RANGE
     if distance < ULTRASOUND_MIN_RANGE:
@@ -59,24 +86,35 @@ def calculate_distance(duration):
 
 # precondition: distance must be between ULTRASOUND_MIN_RANGE and ULTRASOUND_MAX_RANGE
 def calculate_vibration(distance):
+    """Calculates the vibration intensity for the vibration motor.
+
+    Preconditions:
+    distance -- must be in centimetres
+    between ULTRASOUND_MIN_RANGE and ULTRASOUND_MAX_RANGE
+
+    An exponential decay formula is used for a stronger feedback response
+    the closer objects are.
+    """
+
     vibration = e ** (-distance / VIBRATION_EXPONENTIAL_DECAY_CONSTANT)
     return vibration
 
 def motor_ultrasound_pair_driver(trig, echo, motor):
+    """Drives an ultrasound-motor pair."""
     while True:
         duration = get_pulse_time(trig, echo)
         distance = calculate_distance(duration)
-        print(distance)
         vibration_value = calculate_vibration(distance)
         print("vibration is: ", vibration_value)
         try:
             motor.value = vibration_value
-            print("hello")
         except Exception as e:
             print(e)
             pass
 
 if __name__ == '__main__':
-    proc1 = Process(target=motor_ultrasound_pair_driver,
+
+    front_detection = Thread(target=motor_ultrasound_pair_driver, 
                     args=(front_trig, front_echo, front_motor))
-    proc1.start()
+    front_detection.start()
+
